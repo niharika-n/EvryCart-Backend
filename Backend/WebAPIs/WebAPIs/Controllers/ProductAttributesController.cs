231c261115ea9519b6bfb5b22ca172122bd17ede
niharika-n <niharika.niharika@evry.com>
2019-03-15 10:41:38 +0530

    Edited Attributes controller with respect to Result Class.

--- Backend/WebAPIs/WebAPIs/Controllers/ProductAttributesController.cs
+++ Backend/WebAPIs/WebAPIs/Controllers/ProductAttributesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading.Tasks;
@@ -27,164 +28,238 @@ namespace WebAPIs.Controllers
 
         [Authorize(Policy = "AdminOnly")]
         [HttpGet("{id}")]
-        public async Task<IActionResult> Detail(int? id)
+        public async Task<IResult> Detail(int? id)
         {
-            if (id != 0)
+            Result result = new Result();
+            try
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
+                if (id != 0)
+                {                    
+                    var attribute = await context.ProductAttributes.Where(x => x.AttributeID == id).FirstOrDefaultAsync();
+                    if (attribute != null)
+                    {
+                        result.Status = true;
+                        result.Body = attribute;
+                        return result;
+                    }
+                    else
+                    {
+                        result.Message = "Attribute does not exist.";
+                        return result;
+                    }
                 }
-                var attribute = await context.ProductAttributes.Where(x => x.AttributeID == id).FirstOrDefaultAsync();
-                if (attribute != null)
-                {
-                    return Ok(attribute);
-                }
-                else
-                {
-                    return NotFound("Attribute does not exist.");
-                }
+                result.Message = "Attribute ID is not valid.";
+                return result;
             }
-            return Ok();
+            catch (Exception e)
+            {
+                result.StatusCode = HttpStatusCode.InternalServerError;
+                result.Body = e;
+
+                return result;
+            }
         }
 
         [Authorize(Policy = "AdminOnly")]
         [HttpPost]
-        public async Task<IActionResult> InsertAttribute([FromBody] ProductAttribute productAttribute)
+        public async Task<IResult> InsertAttribute([FromBody] ProductAttribute productAttribute)
         {
-            if (!ModelState.IsValid)
+            Result result = new Result();
+            try
             {
-                return BadRequest(ModelState);
-            }
-            var attributeNameCheck = await context.ProductAttributes.Where(x => x.AttributeName == productAttribute.AttributeName).ToListAsync();
-            if (attributeNameCheck.Count() != 0)
-            {
-                return Ok(new { message = "Attribute exists already" });
-            }
-            productAttribute.CreatedDate = DateTime.Now;
-            productAttribute.CreatedBy = helper.GetSpecificClaim("ID");
+                if (!ModelState.IsValid)
+                {
+                    result.StatusCode = HttpStatusCode.BadRequest;
+                    return result;
+                }
+                var attributeNameCheck = await context.ProductAttributes.Where(x => x.AttributeName == productAttribute.AttributeName).ToListAsync();
+                if (attributeNameCheck.Count() != 0)
+                {
+                    result.Body = new { message = "Attribute exists already" };
+                    return result;
+                }
+                productAttribute.CreatedDate = DateTime.Now;
+                productAttribute.CreatedBy = helper.GetSpecificClaim("ID");
 
-            context.ProductAttributes.Add(productAttribute);
-            await context.SaveChangesAsync();
-            ProductAttributeViewModel attributeViewModel = new ProductAttributeViewModel()
+                context.ProductAttributes.Add(productAttribute);
+                await context.SaveChangesAsync();
+                ProductAttributeViewModel attributeViewModel = new ProductAttributeViewModel()
+                {
+                    CreatedUser = helper.GetSpecificClaim("Name")
+                };
+
+                result.Body = new { attribute = productAttribute };
+                result.Status = true;
+                return result;
+            }
+            catch (Exception e)
             {
-                CreatedUser = helper.GetSpecificClaim("Name")
-            };
-            return Ok(new { attribute = productAttribute });
+                result.Body = e;
+                result.StatusCode = HttpStatusCode.InternalServerError;
+                return result;
+            }
         }
 
         [Authorize(Policy = "AdminOnly")]
         [HttpPut]
-        public async Task<IActionResult> UpdateAttribute([FromBody] ProductAttribute productAttribute)
+        public async Task<IResult> UpdateAttribute([FromBody] ProductAttribute productAttribute)
         {
-            if (!ModelState.IsValid)
+            Result result = new Result();
+            try
             {
-                return BadRequest(ModelState);
-            }
-            var attributeObj = context.ProductAttributes.Where(x => x.AttributeID == productAttribute.AttributeID).SingleOrDefault();
-            if (attributeObj == null)
-            {
-                return NotFound("Attribute does not exist");
-            }
-            var attributeNameCheck = await context.ProductAttributes.Where(x => (x.AttributeName == productAttribute.AttributeName) && (x.AttributeID != productAttribute.AttributeID)).ToListAsync();
-            if (attributeNameCheck.Count() != 0)
-            {
-                return Ok(new { message = "Attribute exists already" });
-            }
-            attributeObj.AttributeID = productAttribute.AttributeID;
-            attributeObj.AttributeName = productAttribute.AttributeName;
-            attributeObj.ModifiedDate = DateTime.Now;
-            attributeObj.ModifiedBy = helper.GetSpecificClaim("ID");
-            await context.SaveChangesAsync();
-            ProductViewModel localmodel = new ProductViewModel()
-            {
-                ModifiedUser = helper.GetSpecificClaim("Name")
-            };
-            return Ok(new { attribute = attributeObj });
+                if (!ModelState.IsValid)
+                {
+                    result.StatusCode = HttpStatusCode.BadRequest;
+                    return result;
+                }
+                var attributeObj = context.ProductAttributes.Where(x => x.AttributeID == productAttribute.AttributeID).SingleOrDefault();
+                if (attributeObj == null)
+                {
+                    result.Message = "Attribute does not exist.";
+                    return result;
+                }
+                var attributeNameCheck = await context.ProductAttributes.Where(x => (x.AttributeName == productAttribute.AttributeName) && (x.AttributeID != productAttribute.AttributeID)).ToListAsync();
+                if (attributeNameCheck.Count() != 0)
+                {
+                    result.Body = new { message = "Attribute exists already" };
+                    return result;
+                }
+                attributeObj.AttributeID = productAttribute.AttributeID;
+                attributeObj.AttributeName = productAttribute.AttributeName;
+                attributeObj.ModifiedDate = DateTime.Now;
+                attributeObj.ModifiedBy = helper.GetSpecificClaim("ID");
+                await context.SaveChangesAsync();
+                ProductViewModel localmodel = new ProductViewModel()
+                {
+                    ModifiedUser = helper.GetSpecificClaim("Name")
+                };
+                result.Status = true;
+                result.Body = new { attribute = attributeObj };
 
+                return result;
+            }
+            catch (Exception e)
+            {
+                result.StatusCode = HttpStatusCode.InternalServerError;
+                result.Body = e;
+
+                return result;
+            }
         }
 
         [Authorize(Policy = "AdminOnly")]
         [HttpGet]
-        public async Task<IActionResult> Listing(DataHelperModel dataHelper, bool getAll)
+        public async Task<IResult> Listing(DataHelperModel dataHelper, bool getAll)
         {
-            var listQuery = from attribute in context.ProductAttributes
-                            join createdUser in context.Login
-                            on attribute.CreatedBy equals createdUser.UserID
-                            into createname
-                            from createdUsername in createname.DefaultIfEmpty()
-                            let createdByUser = createdUsername.Username
-                            join modifiedUser in context.Login
-                            on attribute.ModifiedBy equals modifiedUser.UserID
-                            into modifyname
-                            from modifiedUsername in modifyname.DefaultIfEmpty()
-                            let modifiedByUser = modifiedUsername.Username
-                            join Values in context.ProductAttributeValues
-                            on attribute.AttributeID equals Values.AttributeID
-                            into attributeValuesCount
-                            from attributeValues in attributeValuesCount.DefaultIfEmpty()                               
-                            group new { attributeValues, attribute, createdByUser, modifiedByUser } by
-                            new { attribute, createdByUser, modifiedByUser } into valuesCount
-                            select new ProductAttributeViewModel
-                            {
-                                AttributeID = valuesCount.Key.attribute.AttributeID,
-                                AttributeName = valuesCount.Key.attribute.AttributeName,
-                                CreatedBy = valuesCount.Key.attribute.CreatedBy,
-                                AssociatedProductValues = valuesCount.Where(x => x.attributeValues!= null? x.attributeValues.AttributeID == x.attribute.AttributeID : false).Count(),
-                                CreatedDate = valuesCount.Key.attribute.CreatedDate,
-                                ModifiedBy = valuesCount.Key.attribute.ModifiedBy,
-                                ModifiedDate = valuesCount.Key.attribute.ModifiedDate,
-                                CreatedUser = valuesCount.Key.createdByUser,
-                                ModifiedUser = valuesCount.Key.modifiedByUser
-                            };
-            if (!getAll)
+            Result result = new Result();
+            try
             {
-                if (dataHelper.Search != null)
+                var listQuery = from attribute in context.ProductAttributes
+                                join createdUser in context.Login
+                                on attribute.CreatedBy equals createdUser.UserID
+                                into createname
+                                from createdUsername in createname.DefaultIfEmpty()
+                                let createdByUser = createdUsername.Username
+                                join modifiedUser in context.Login
+                                on attribute.ModifiedBy equals modifiedUser.UserID
+                                into modifyname
+                                from modifiedUsername in modifyname.DefaultIfEmpty()
+                                let modifiedByUser = modifiedUsername.Username
+                                join Values in context.ProductAttributeValues
+                                on attribute.AttributeID equals Values.AttributeID
+                                into attributeValuesCount
+                                from attributeValues in attributeValuesCount.DefaultIfEmpty()
+                                group new { attributeValues, attribute, createdByUser, modifiedByUser } by
+                                new { attribute, createdByUser, modifiedByUser } into valuesCount
+                                select new ProductAttributeViewModel
+                                {
+                                    AttributeID = valuesCount.Key.attribute.AttributeID,
+                                    AttributeName = valuesCount.Key.attribute.AttributeName,
+                                    CreatedBy = valuesCount.Key.attribute.CreatedBy,
+                                    AssociatedProductValues = valuesCount.Where(x => x.attributeValues != null ? x.attributeValues.AttributeID == x.attribute.AttributeID : false).Count(),
+                                    CreatedDate = valuesCount.Key.attribute.CreatedDate,
+                                    ModifiedBy = valuesCount.Key.attribute.ModifiedBy,
+                                    ModifiedDate = valuesCount.Key.attribute.ModifiedDate,
+                                    CreatedUser = valuesCount.Key.createdByUser,
+                                    ModifiedUser = valuesCount.Key.modifiedByUser
+                                };
+                if (!getAll)
                 {
-                    listQuery = listQuery.Where(x => x.AttributeName.Contains(dataHelper.Search));
+                    if (dataHelper.Search != null)
+                    {
+                        listQuery = listQuery.Where(x => x.AttributeName.Contains(dataHelper.Search));
+                    }
+                    var list = listQuery;
+                    list = DataSort.SortBy(list, dataHelper.SortColumn, dataHelper.SortOrder);
+                    var resultCount = list.Count();
+                    var pagedList = DataCount.Page(list, dataHelper.PageNumber, dataHelper.PageSize);
+                    var resultList = await pagedList.ToListAsync();
+                    ResultModel resultModel = new ResultModel();
+                    resultModel.ProductAttributeResult = resultList;
+                    resultModel.TotalCount = resultCount;
+                    if (resultList.Count == 0)
+                    {
+                        result.Message = "No records present.";
+                        return result;
+                    }
+
+                    result.Status = true;
+                    result.Body = resultModel;
+                    return result;
                 }
-                var list = listQuery;
-                list = DataSort.SortBy(list, dataHelper.SortColumn, dataHelper.SortOrder);
-                var resultCount = list.Count();
-                var pagedList = DataCount.Page(list, dataHelper.PageNumber, dataHelper.PageSize);
-                var resultList = await pagedList.ToListAsync();
-                ResultModel result = new ResultModel();
-                result.ProductAttributeResult = resultList;
-                result.TotalCount = resultCount;
-                if (resultList.Count == 0)
+                else
                 {
-                    return NotFound("No records present.");
+                    listQuery = listQuery.OrderBy(x => x.AttributeName);
+                    var attributeList = await listQuery.ToListAsync();
+                    result.Body = attributeList;
+                    result.Status = true;
+                    return result;
                 }
-                return Ok(result);
             }
-            else
+            catch (Exception e)
             {
-                listQuery = listQuery.OrderBy(x => x.AttributeName);
-                var attributeList = await listQuery.ToListAsync();
-                return Ok(attributeList);
+                result.StatusCode = HttpStatusCode.InternalServerError;
+                result.Body = e;
+
+                return result;
             }
         }
 
         [Authorize(Policy = "AdminOnly")]
         [HttpDelete]
-        public async Task<IActionResult> Delete(int Id)
+        public async Task<IResult> Delete(int Id)
         {
-            var deleteQuery = await context.ProductAttributes.Where(x => x.AttributeID == Id).FirstOrDefaultAsync();
-            if (deleteQuery == null)
+            Result result = new Result();
+            try
             {
-                return NotFound("Attribute does not exist.");
-            }
-            context.ProductAttributes.Remove(deleteQuery);
-            await context.SaveChangesAsync();
-            var deletedAttribute = await context.ProductAttributes.Where(x => x.AttributeID == Id).FirstOrDefaultAsync();
-            if (deletedAttribute == null)
-            {
-                var attributeValues = await context.ProductAttributeValues.Where(x => x.AttributeID == Id).ToListAsync();
-                context.ProductAttributeValues.RemoveRange(attributeValues);
+                var deleteQuery = await context.ProductAttributes.Where(x => x.AttributeID == Id).FirstOrDefaultAsync();
+                if (deleteQuery == null)
+                {
+                    result.Message = "Attribute does not exist.";
+                    return result;
+                }
+                context.ProductAttributes.Remove(deleteQuery);
                 await context.SaveChangesAsync();
-            }
-            return Ok("Deleted successfully.");
-        }
+                var deletedAttribute = await context.ProductAttributes.Where(x => x.AttributeID == Id).FirstOrDefaultAsync();
+                if (deletedAttribute == null)
+                {
+                    var attributeValues = await context.ProductAttributeValues.Where(x => x.AttributeID == Id).ToListAsync();
+                    context.ProductAttributeValues.RemoveRange(attributeValues);
+                    await context.SaveChangesAsync();
+                }
+                result.Status = true;
+                result.Message = "Deleted successfully.";
 
+                return result;
+            }
+
+            catch (Exception e)
+            {
+                result.StatusCode = HttpStatusCode.InternalServerError;
+                result.Body = e;
+
+                return result;
+            }
+        }
     }
 }
